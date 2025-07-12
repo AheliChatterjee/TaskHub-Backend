71056a263ac87d0f85a483c750ce5ea9ed152294
Divyarishi Mohapatra <123584270+rishi2504537@users.noreply.github.com>
2025-07-12 14:10:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,10 +30,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: [
-    "http://localhost:5173",                    
-    "https://task-hub-frontend-three.vercel.app" 
-  ]
+    "http://localhost:5173",
+    "https://task-hub-frontend-three.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 }));
+
 
 app.use(async (req, res, next) => {
   await connectToDB();

d4659bc08c3af23c743b2e4de88fe76c737fda78
u4ia-abhra <abhrajitgh@gmail.com>
2025-12-26 12:02:53 +0530

    Added new domain to cors

--- server.js
+++ server.js
@@ -41,7 +41,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: [
     "http://localhost:5173",                    
-    "https://task-hub-frontend-three.vercel.app" 
+    "https://task-hub-frontend-three.vercel.app",
+    "https://taskhub.digital/"
   ]
 }));
 

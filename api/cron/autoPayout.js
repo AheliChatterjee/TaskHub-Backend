561d5320dab502540a3301ca9101dc3bd4fa9163
Aheli Chatterjee <chatterjeeaheli2005@gmail.com>
2025-12-30 10:45:00 +0530

    Fix serverless crash: lazy Razorpay env validation

--- api/cron/autoPayout.js
+++ api/cron/autoPayout.js
@@ -1,8 +1,8 @@
-import mongoose from "mongoose";
-import Task from "../../models/task.js";
-import { payoutToFreelancer } from "../../services/payoutService.js";
+const mongoose = require("mongoose");
+const Task = require("../../models/task");
+const { payoutToFreelancer } = require("../../services/payoutService");
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   try {
     // Safety: env check
     if (!process.env.MONGODB_URL) {
